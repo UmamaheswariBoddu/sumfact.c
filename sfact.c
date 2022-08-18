e0b4139b5b64afe8e88e894d00998a35e185e0c6
UmamaheswariBoddu <111072780+UmamaheswariBoddu@users.noreply.github.com>
2022-08-18 20:07:01 +0530

    Add files via upload

---
+++ sfact.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,n,sum;
+	long fact=1;
+     
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+     
+    sum=0;
+
+    for(i=1;i<=n;i++)
+        {
+		fact = fact * i;
+		sum= sum+ fact;
+		}
+
+    printf("The Sum of the series 1!+2!+3!+...+%d! is: %d\n",n,sum);
+     
+    return 0;
+}
+___________________________________________
+OUTPUT:
+Enter the value of n: 5
+The sum of the series 1!+2!+3!+...+5! is: 153
